fce2156d6452085a52e3026784c6d481f2f45179
Rick Cable <rick.cable@gmail.com>
2024-04-22 11:17:28 -0700

    Add project files.

---
+++ Selenium_Web_Scraper_Example/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace WebScraper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://Google.com/");
+            var element = driver.FindElement(By.XPath("//*[@id=\"APjFqb\"]"));
+            element.SendKeys("Webshop");
+            element.Submit();
+            var titles = driver.FindElements(By.XPath("//h3"));
+
+            foreach (var title in titles)
+            {
+                //Output
+                Console.WriteLine(title.Text);
+
+
+
+            }
+
+        }
+    }
+}
+
